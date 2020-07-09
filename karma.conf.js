40281278c839b9ad2fcbf78a8913fbef0980c1f7
thienqle-fullstack <thienqle.vn@gmail.com>
2020-07-09 19:20:13 -0400

    remove PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      //require('karma-chrome-launcher'), /* disable the karma-chrome-launcher*/
-      require('karma-phantomjs-launcher'),  /* add this to disable the karma-phantomjs-launcher*/
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -26,10 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ['Chrome'],
-    browsers: ['PhantomJS'],
+    customLaunchers: {
+      ChromeHeadless: {
+       base: 'Chrome',
+       flags: [
+       '--headless','--disable-gpu',
+       '--no-sandbox',
+       '--remote-debugging-port=9222']
+       }
+      },
+    browsers: ['ChromeHeadless'],
+    //browsers: ['Chrome'],
     singleRun: true,
-    // singleRun: false,
     restartOnFileChange: true
   });
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
